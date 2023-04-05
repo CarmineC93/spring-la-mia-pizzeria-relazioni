a231c4b436b6ce319046512a986df04e48762cf3
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-04-05 14:18:49 +0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizzas (name, description, price) VALUES('capricciosa', 'roba mista', 16.00);
+INSERT INTO pizzas (name, description, price) VALUES('margherita', 'roba semplice', 10.00);
+INSERT INTO pizzas (name, description, price) VALUES('diavola', 'roba piccantina', 15.00);
